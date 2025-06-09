e18f086778b663e28c997aa4fea5781a4b699eaf
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-06-09 17:08:21 -0300

    feat(packages): add `thunar` package

--- scripts/02_packages.sh
+++ scripts/02_packages.sh
@@ -1,7 +1,8 @@
 PACKAGES=(
-	asdf-vm bashtop bat beekeeper-studio-bin brave-bin curl discord docker docker-buildx docker-compose eza fish fzf
-	kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji polybar python-pywal ripgrep slack-desktop
-	the_silver_searcher timeshift tldr tmux ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
+	asdf-vm bashtop bat beekeeper-studio-bin brave-bin curl discord docker docker-buildx
+    docker-compose eza fish fzf	kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji polybar
+    python-pywal ripgrep slack-desktop the_silver_searcher thunar timeshift tldr tmux
+    ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
 )
 
 for package in "${PACKAGES[@]}"; do
