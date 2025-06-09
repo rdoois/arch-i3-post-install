12c1d50c3341ae42ff67ae7ce0eb1d5ac7864dc5
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-06-09 09:36:12 -0300

    feat(packages): add `neofetch` package

--- 02_packages.sh
+++ 02_packages.sh
@@ -1,7 +1,7 @@
 PACKAGES=(
 	asdf-vm bashtop bat beekeeper-studio-bin brave-bin curl discord docker docker-buildx docker-compose eza fish fzf
-	kitty lazygit less neovim nitrogen noto-fonts-emoji ripgrep slack-desktop the_silver_searcher timeshift	tldr tmux 
-	ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip
+	kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji ripgrep slack-desktop the_silver_searcher timeshift 
+	tldr tmux ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip
 )
 
 for package in "${PACKAGES[@]}"; do
