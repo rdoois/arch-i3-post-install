08ee95af05bb1f7b40f0c92a938e4c848839eb48
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-06-10 16:09:09 -0300

    feat(packages): remove `slack-desktop` package

--- scripts/02_packages.sh
+++ scripts/02_packages.sh
@@ -1,8 +1,8 @@
 PACKAGES=(
 	asdf-vm bashtop bat beekeeper-studio-bin brave-bin curl discord docker docker-buildx
     docker-compose eza fish fzf	kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji polybar
-    python-pywal ripgrep scrot slack-desktop the_silver_searcher thunar timeshift tldr tmux
-    ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
+    python-pywal ripgrep scrot the_silver_searcher thunar timeshift tldr tmux ttf-firacode-nerd
+    ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
 )
 
 for package in "${PACKAGES[@]}"; do
