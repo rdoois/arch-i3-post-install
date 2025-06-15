7e6758d4f8f1fe969b5fa6c8a5b897b7b7f22c4a
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-06-14 21:14:21 -0300

    feat(packages): add `pavu-control` package

--- scripts/02_packages.sh
+++ scripts/02_packages.sh
@@ -1,8 +1,8 @@
 PACKAGES=(
     asdf-vm bashtop bat beekeeper-studio-bin brave-bin curl discord docker docker-buildx
-    docker-compose eza fish fzf kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji polybar
-    python-pywal ripgrep scrot the_silver_searcher thunar timeshift tldr tmux ttf-firacode-nerd
-    ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
+    docker-compose eza fish fzf	kitty lazygit less neofetch neovim nitrogen noto-fonts-emoji
+    pavucontrol polybar python-pywal ripgrep scrot the_silver_searcher thunar timeshift tldr tmux
+    ttf-firacode-nerd ttf-jetbrains-mono-nerd unzip wget xorg-xrandr xsel yq zip zoom
 )
 
 for package in "${PACKAGES[@]}"; do
